b57e69c758d1764116d14f2842e68e02ab2f5a14
Sebastian Mendez <sebastian@nyaya.tech>
2026-07-20 01:50:39 +0200

    Silence compiler warnings from missing funcions from outside packages.

--- early-init.el
+++ early-init.el
@@ -1,6 +1,7 @@
 (setq gc-cons-threshold most-positive-fixnum
         gc-cons-percentage 0.6)
   (setq package-enable-at-startup nil)
+  (setq native-comp-async-report-warnings-errors 'silent)
   (push '(menu-bar-lines . 0) default-frame-alist)
   (push '(tool-bar-lines . 0) default-frame-alist)
   (push '(vertical-scroll-bars) default-frame-alist)
